bd2ad549fa2f1fa3a3bd3cd4c4c1f597ad650a2d
Shoaib Ali <shoaibali26021999@gmail.com>
2022-09-10 18:31:57 +0530

    Project completed

--- app/src/main/java/com/example/retrofitdemo/AlbumService.kt
+++ app/src/main/java/com/example/retrofitdemo/AlbumService.kt
@@ -1,7 +1,7 @@
 package com.example.retrofitdemo
 
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 
 // service interface
@@ -10,4 +10,18 @@ interface AlbumService {
 
     @GET("/albums")
     suspend fun getAlbums():Response<Albums>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId:Int):Response<Albums>
+
+
+// path parameter to get single object
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") albumId:Int):Response<AlbumsItem>
+
+    @POST(" /albums")
+    suspend fun uploadAlbum(@Body album:AlbumsItem):Response<AlbumsItem>
+
+
 }
--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var textView: TextView
+    private lateinit var retService: AlbumService
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,34 +26,77 @@ class MainActivity : AppCompatActivity() {
 
 
         // we need to have a instance of  Service Interface we will create retrofit Instance class
-        val retService=RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+         retService=RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+         //getRequestWithQueryParameters()
+        // getRequestWithPathParameters()
+        uploadAlbum()
 
-        // using corutine livedata builder we are going to get Retrofit response as livedata
+    }
+
+    private fun getRequestWithQueryParameters()
+    {
 
         val responseLiveData:LiveData<Response<Albums>> = liveData {
-            val response=retService.getAlbums()
+            val response=retService.getSortedAlbums(3)
             emit(response)
         }
 
 
         responseLiveData.observe(this,Observer{
-             val albumsList=it.body()?.listIterator()
+            val albumsList=it.body()?.listIterator()
             if(albumsList!=null)
             {
                 while(albumsList.hasNext())
                 {
                     val albumsItem=albumsList.next()
                     val result = " "+"Album Title: ${albumsItem.title}" +"\n"+
-                                 " "+"Album id : ${albumsItem.id}" +"\n"+
-                                 " "+"User id : ${albumsItem.userId}" +"\n\n\n"
+                            " "+"Album id : ${albumsItem.id}" +"\n"+
+                            " "+"User id : ${albumsItem.userId}" +"\n\n\n"
 
                     textView.append(result)
                 }
             }
         })
 
+    }
+
+    private fun getRequestWithPathParameters()
+    {
+
+
+        val pathResponse:LiveData<Response<AlbumsItem>> = liveData {
+            val response=retService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this,Observer{
+            val title=it.body()?.title
+            Toast.makeText(applicationContext,title,Toast.LENGTH_LONG).show()
+        })
+
 
 
 
     }
+
+    private fun uploadAlbum()
+    {
+        val album = AlbumsItem(0,"MY title",3)
+
+        val postResponse:LiveData<Response<AlbumsItem>> = liveData {
+            val response =retService.uploadAlbum(album)
+            emit(response)
+        }
+
+        postResponse.observe(this,Observer{
+            val receivedAlbumsItem = it.body()
+            val result = " "+"Album Title: ${receivedAlbumsItem?.title}" +"\n"+
+                    " "+"Album id : ${receivedAlbumsItem?.id}" +"\n"+
+                    " "+"User id : ${receivedAlbumsItem?.userId}" +"\n\n\n"
+            textView.text=result
+
+        })
+
+    }
+
 }
